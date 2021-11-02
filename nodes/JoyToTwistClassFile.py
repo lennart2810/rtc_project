9b471dc53204c9803ba49212ababab11b9a0ae8c
lennart fuhrig <fuhrig.lennart@web.de>
2021-11-02 15:59:43 +0100

    params

--- nodes/JoyToTwistClassFile.py
+++ nodes/JoyToTwistClassFile.py
@@ -29,34 +29,31 @@ from sensor_msgs.msg import Joy
 class JoyToTwist(object):
     def __init__(self):
 
-        self.pub_flag = True
-
         self.buttons_dict = {"options": 9}
-
         self.axes_dict = {"L3Y": 1, "R3X": 3}
 
         rospy.init_node('joy_to_twist_node', anonymous=True)
-
         rospy.Subscriber('joy', Joy, self.joy_callback)
+        self.vel_pub = rospy.Publisher('/cmd_vel',Twist, queue_size=10)
 
-        self.velocity_publisher = rospy.Publisher('/cmd_vel',
-                                                  Twist, queue_size=10)
-
+        self.pub_flag = True
         self.vel_msg = Twist()
-
         self.rate = rospy.Rate(10)
+
+        # Scales aus JoyToTurtleTwist.yaml <-- rosparams werden im launch-file übergeben
+        self.scales = rospy.get_param('~scales')
 
         while not rospy.is_shutdown():
 
             if self.pub_flag:
-                self.velocity_publisher.publish(self.vel_msg)
+                self.vel_pub.publish(self.vel_msg)
                 self.rate.sleep()
 
     def joy_callback(self, data):
 
-        # "enable bzw. disable" velocity_publisher
+        # "enable bzw. disable" vel_pub
         if data.buttons[self.buttons_dict["options"]]:
             self.pub_flag = not self.pub_flag
 
-        self.vel_msg.linear.x = data.axes[self.axes_dict["L3Y"]] * 0.5
-        self.vel_msg.angular.z = data.axes[self.axes_dict["R3X"]] * 1.5
+        self.vel_msg.linear.x = data.axes[self.axes_dict["L3Y"]] * self.scales["linear"]["x"]
+        self.vel_msg.angular.z = data.axes[self.axes_dict["R3X"]] * self.scales["angular"]["z"]
