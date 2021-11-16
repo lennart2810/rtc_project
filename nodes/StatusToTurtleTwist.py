b5c54c61b752cd3a4cfc42f031fe6fdbe572914d
lennart2810 <lennart.fuhrig@web.de>
2021-11-16 12:28:19 +0100

    test

--- nodes/StatusToTurtleTwist.py
+++ nodes/StatusToTurtleTwist.py
@@ -12,6 +12,7 @@ from geometry_msgs.msg import Twist
 from ds4_driver.msg import Status, Feedback
 from sensor_msgs.msg import LaserScan
 from math import isnan
+#teswt 
 
 
 class StatusToTurtleTwist(object):
